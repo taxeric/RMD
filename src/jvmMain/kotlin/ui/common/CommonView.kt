7ed19e26334b8acb7baae84d9517089dabbed4b9
taxeric <51876689+taxeric@users.noreply.github.com>
2022-09-01 16:16:03 +0800

    [update]: 分包 [feat]: 新增通用实现

---
+++ src/jvmMain/kotlin/ui/common/CommonView.kt
@@ -0,0 +1,20 @@
+package ui.common
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Divider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun verticalDivider(width: Dp = 1.dp, color: Color = Color.Gray){
+    Divider(modifier = Modifier.width(width), color = color)
+}
+
+@Composable
+fun horizontalDivider(height: Dp = 1.dp, color: Color = Color.Gray){
+    Divider(modifier = Modifier.height(height), color = color)
+}
